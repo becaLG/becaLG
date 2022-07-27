92b2ef716b244159f9bfa308f0d9e036b4203de9
rongshichao <2011991957@qq.com>
2022-07-27 23:18:33 +0800

    Dialog

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,31 +1,64 @@
 <template>
     <div id="home">
-        <van-button @click="handelClick" >开关弹窗</van-button>
-        <myDialog :visible="visible" title="home的标题" content="home的内容">
-          
+        <van-button @click="handelClick" class="swithup">开关弹窗</van-button>
+        <myDialog :visible="visible" title="提示" content="这是一段信息">
+            <div>sdfsdf</div>
+            <h1 class="h1">12312
+            </h1>
              <template #footer>
-                    
-                    <van-button>取消1</van-button>
-                    
-               
-                    <van-button>确定1</van-button>
+                    <van-button class="Cancel">取1消</van-button>
+                    <van-button class="Sure">确1定</van-button>
              </template>
         </myDialog>
+
+        <div class="background-style" v-show="background">
+   
+        </div>
         
     </div>
 </template>
 
 <script setup>
-    import myDialog from '@/views/order/Dialog.vue';
+    import myDialog from '../order/Dialog.vue';
     import { ref } from 'vue';
     const visible = ref(false);
-
+    const background = ref(false);
+    
     const handelClick = () => {
         visible.value = !visible.value;
+        background.value = !background.value;
     }
-    
+
 </script>
 
 <style scoped>
+.background-style {
+      background-color: rgba(128, 128, 128, .5); /* 设置背景颜色 */
+      position:fixed;
+      top:0;
+      left:0;
+      bottom:0;
+      right:0;
+      z-index:-1;
+}
+.Cancel{
+    color: #606266;
+    position: relative;
+    left: 110px;
+    top: 10px;
+    border-radius:5px;
+    width: 75px;
+    height: 40px;
+}
+.Sure{
+    background: #409eff;
+    color: white;
+    position: relative;
+    left: 130px;
+    border-radius:5px;
+    width: 75px;
+    height: 40px;
+    top: 10px;
+}
 
 </style>    
---
+++ src/views/order/Dialog.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="my-dialog-top" v-show="visible">
+        <div class="title">{{title}}</div>
+        <div class="content"><slot>{{content}}</slot></div>
+        <slot name="footer" class="asd">
+            <van-button>取2消</van-button>
+            <van-button>确2定</van-button>
+        </slot>
+        
+    </div>
+</template>
+
+<script setup>
+    const props = defineProps({
+        visible:{
+            type:Boolean,
+            default:false
+        },
+        title:{
+            type:String,
+            default:'这是Dialog的标题'
+        },
+        content:{
+            type:String,
+            default:"这是Dialog内容"
+        }
+    }) 
+</script>
+
+<style scoped>
+.my-dialog-top{
+    width: 460px;
+    height: 200px;
+    background: white;
+    z-index: 1;
+    margin: 30px auto;
+    position: relative;
+}
+.title{
+    width: 470px;
+    height: 54px;
+    font-size: 18px;
+    position: relative;
+    top: 21px;
+    right: 199px
+}
+.content{
+    width: 460px;
+    height: 80px;
+    position: relative;
+    top: 21px;
+    right: 190px
+}
+
+</style>
